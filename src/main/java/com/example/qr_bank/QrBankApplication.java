5af08514429d4c8b873efb1a486d80d81ede5514
ORHAN TÜRKMENOĞLU <orhantrkmn749@gmail.com>
2025-05-15 20:18:44 +0300

    first commit

---
+++ src/main/java/com/example/qr_bank/QrBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.qr_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrBankApplication.class, args);
+	}
+
+}
