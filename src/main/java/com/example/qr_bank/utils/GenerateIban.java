2ff2aa0340178064a71c1b8ec5341cd91a84bb75
ORHAN TÜRKMENOĞLU <orhantrkmn749@gmail.com>
2025-05-28 21:50:43 +0300

    feat: implement IBAN generator with properly formatted numeric groups (4-4-4-4-4-2) for Turkey

--- src/main/java/com/example/qr_bank/utils/GenerateIban.java
+++ src/main/java/com/example/qr_bank/utils/GenerateIban.java
@@ -1,11 +1,27 @@
 package com.example.qr_bank.utils;
 
-import java.util.UUID;
-
 public class GenerateIban {
 
     public static String generateIban() {
-        return "TR" + UUID.randomUUID().toString().replace("-", "").substring(0, 24);
+        String countryCode = "TR";
+        String checkDigits = "00";
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 22; i++) {
+            int digit = (int) (Math.random() * 10);
+            builder.append(digit);
+        }
+
+        String rawDigits = builder.toString();
+
+        String formatted = rawDigits.substring(0,4)+" "+
+                rawDigits.substring(4,8)+" "+
+                rawDigits.substring(8,12)+" "+
+                rawDigits.substring(12,16)+" "+
+                rawDigits.substring(16,20)+" "+
+                rawDigits.substring(20,22);
+
+        return countryCode + checkDigits +" "+formatted;
     }
 
 }
